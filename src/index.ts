0fd5862ff5935df915b7e1fea7f2262c3e5630a0
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-08-27 11:00:10 -0300

    feat: instala e configura pacote dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import app from './server';
+import dotenv from 'dotenv';
 
-const port = process.env.port || '3001';
+dotenv.config();
+
+const port = process.env.PORT || '3001';
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
