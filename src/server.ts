d0a83a748da1c873d42c7d0ccc077a9021b5f5d3
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-08-27 10:54:30 -0300

    feat: instala swagger e configura

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import express from 'express';
 import { Request, Response } from 'express';
-
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerDocument from './swagger.json';
 
 const app = express();
+
+const options = {
+    definition: swaggerDocument,
+    apis: ['./src/routes/*.ts'],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get('/', (_req: Request, res: Response) =>
     res.json({ message: 'active server' }),
