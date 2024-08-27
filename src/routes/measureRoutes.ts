f38d9b42c0f0717e52c550ef00e2061f79d537dc
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-08-27 16:48:18 -0300

    feat: cria middleware de verificação de customer code
    verifica se o campo foi efetivamente preenchido e se é do tipo string

--- src/routes/measureRoutes.ts
+++ src/routes/measureRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import validateImageBase64 from '../middlewares/validateImageBase64';
+import validateCustomerCode from '../middlewares/validateCustomerCode';
 
 const measuresRouter = Router();
 
 measuresRouter.route('/upload').post(
     validateImageBase64,
-    validateCustomeCode,
+    validateCustomerCode,
     validateMeasureDateTime,
     validateMeaureType,
     MeasureController.createMeasure
