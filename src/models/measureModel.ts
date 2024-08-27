c2d4150f13e7ac71a9f7be47f3dc73e8450b7319
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-08-27 19:38:16 -0300

    feat: cria método de criação de medição na model

---
+++ src/models/measureModel.ts
@@ -0,0 +1,21 @@
+import { IMeasure } from '../interfaces/IMeasure';
+import { PrismaClient } from '@prisma/client';
+
+export default class MeasureModel {
+    private prismaClient = new PrismaClient()
+
+    async create(measure: IMeasure, value: number, dataImage: any): Promise<IMeasure> {
+        return await this.prismaClient.measure.create({
+            data: {
+                customerCode: measure.customerCode,
+                measureDatetime: new Date(measure.measureDatetime),
+                measureType: measure.measureType, 
+                measureValue: value,
+            },
+            include: {
+                image: dataImage
+            }
+       })
+    }
+
+}
