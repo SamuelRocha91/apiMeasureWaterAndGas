b488124a3e83cecaf04eb3105c0e67ed34b881bc
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-08-28 15:57:26 -0300

    feat: adapta envios da service com uso das interfaces corretas

--- src/services/measureService.ts
+++ src/services/measureService.ts
@@ -1,3 +1,4 @@
+import { ICreateMeasure, IMeasureDate } from "../interfaces/ICreateMeasure";
 import { IMeasure } from "../interfaces/IMeasure";
 import { ServiceResponse } from "../interfaces/IServiceResponse";
 import MeasureModel from "../models/measureModel";
@@ -7,15 +8,15 @@ export default class MeasureService {
         private measureModel = new MeasureModel(),
     ) { }
 
-    public async createMeasure(measure: IMeasure): Promise<ServiceResponse<IMeasure>> {
-        const allMeasure = await this.measureModel.findAllByCode(measure.customerCode);
-        if (allMeasure.some((measureMonth: IMeasure ) => measureMonth.measureDatetime.getFullYear() === measure.measureDatetime.getFullYear() &&
+    public async createMeasure(measure: IMeasure): Promise<ServiceResponse<ICreateMeasure>> {
+        const allMeasure = await this.measureModel.findAllByCode(measure.customerCode, measure.measureType);
+        if (allMeasure.some((measureMonth: IMeasureDate ) => measureMonth.measureDatetime.getFullYear() === measure.measureDatetime.getFullYear() &&
             measureMonth.measureDatetime.getMonth() === measure.measureDatetime.getMonth())) {
             return {
                 status: 'DOUBLE_REPORT', message: "Leitura do mês já realizada"
             }
         }
-        const newMeasure = this.measureModel.create(measure, 1, 0)
+        const newMeasure = await this.measureModel.create(measure, 1, 0)
         return { status: 'SUCCESSFUL', message: newMeasure };
     }
 }
