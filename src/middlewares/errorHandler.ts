f2e36b606acad26f9282df7c43c6663e908925e6
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-08-29 20:19:21 -0300

    feat: cria escutador de evento de erro

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
+import BaseError from '../exceptions/BaseError';
+import InternalServerException from '../exceptions/InternalServerException';
+
+const normalizeError = (err: Error) => {
+  if (err instanceof BaseError) {
+    return err;
+  }
+
+  return new InternalServerException(err);
+};
+
+const ErrorHandler = (
+  error: Error,
+  _req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _next: NextFunction
+) => {
+  const baseError = normalizeError(error);
+
+  const { message, statusCode, typeError } = baseError.getBody();
+
+  return res.status(statusCode).json({ 
+    error_code: typeError,
+    error_description: message
+  });
+};
+
+export default ErrorHandler;
