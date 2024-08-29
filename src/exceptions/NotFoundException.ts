fb3e41f6a4bb93c8de89fce63f4be013bdc39204
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-08-29 20:36:35 -0300

    feat: cria uma exceção personalizada para dados não encontrados

---
+++ src/exceptions/NotFoundException.ts
@@ -0,0 +1,8 @@
+import { httpStatus } from '../utils/httpStatus.utils';
+import BaseError from './BaseError';
+
+export default class NotFoundException extends BaseError {
+  constructor(typeError: string, message: string) {
+    super(httpStatus.NOT_FOUND, typeError, message);
+  }
+}
