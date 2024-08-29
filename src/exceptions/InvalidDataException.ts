57bd0dd3bfd3f8cfd91b7b35efb5284d67b498ee
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-08-29 20:34:02 -0300

    feat: cria exceção personalizada para dados ou parâmetros inválidos

---
+++ src/exceptions/InvalidDataException.ts
@@ -0,0 +1,8 @@
+import { httpStatus } from '../utils/httpStatus.utils';
+import BaseError from './BaseError';
+
+export default class InvalidDataException extends BaseError {
+  constructor(typeError: string, message: string) {
+    super(httpStatus.BAD_REQUEST, typeError, message);
+  }
+}
