ce83b3030ab9dec811543df6ba1dce511fcf62ad
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-08-29 20:13:52 -0300

    feat: cria exceção customizada para erros de server

---
+++ src/exceptions/InternalServerException.ts
@@ -0,0 +1,14 @@
+import BaseError from './BaseError';
+import { httpStatus } from '../utils/httpStatus.utils';
+
+export default class InternalServerException extends BaseError {
+  constructor(err: Error) {
+    super(httpStatus.INTERNAL_SERVER_ERROR, "SERVER_ERROR", 'Internal server error');
+    
+    console.error({
+      message: err.message,
+      stackTrace: err.stack,
+      level: 'fatal'
+    });
+  }
+}
