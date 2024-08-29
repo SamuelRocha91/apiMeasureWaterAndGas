478a765c2adb45dbb6f2161983d37cd1512f315b
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-08-29 12:59:00 -0300

    feat: cria teste para middleware de validação de imagem base64

--- src/tests/measures.test.ts
+++ src/tests/measures.test.ts
@@ -3,7 +3,7 @@ import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import chaisAsPromised from 'chai-as-promised';
 import app from '../server';
-import  invalidMeasureType  from './mocks/measures.mock'
+import  mockMeasure  from './mocks/measures.mock'
 import { httpStatus } from '../utils/httpStatus.utils';
 
 chai.use(chaisAsPromised);
@@ -18,7 +18,24 @@ describe('POST /upload', function () {
     it('ao receber uma requisição com um tipo de medição incorreto, retorne um erro',
       async function () {
         // Arrange
-        const httpRequestBody = invalidMeasureType.invalidMeasureType;
+        const httpRequestBody = mockMeasure.invalidMeasureType;
+
+        //Act
+        const httpResponse = await chai
+          .request(app)
+          .post('/upload')
+          .send(httpRequestBody);
+
+        //Assert
+        expect(httpResponse.status).to.equal(httpStatus.BAD_REQUEST);
+        expect(httpResponse.body.error_code).to.equal("INVALID_DATA");
+        expect(httpResponse.body.error_description)
+          .to.equal("Os dados fornecidos no corpo da requisição são inválidos");
+      })    
+    it('ao receber uma requisição com uma imagem base64 incorreta, retorne um erro',
+      async function () {
+        // Arrange
+        const httpRequestBody = mockMeasure.invalidImageBase64;
 
         //Act
         const httpResponse = await chai
