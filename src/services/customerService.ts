1deefe2fb3dd6136a1f334fa745095aa1badfe17
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-16 00:10:25 -0300

    feat: cria service de customer com método create

---
+++ src/services/customerService.ts
@@ -0,0 +1,13 @@
+import { ServiceResponse } from "../interfaces/IServiceResponse";
+
+export default class CustomerService {
+  constructor(
+    private customerModel = new CustomerModel()
+  ) { }
+
+  public async create(data: ICustomer): Promise<ServiceResponse<ICreateCustomer>> {
+    const customerSave = await this.customerModel.create(data);
+
+    return { status: "SUCCESSFUL", message: customerSave };
+  }
+}
