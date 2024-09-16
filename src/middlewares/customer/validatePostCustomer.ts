db94b3bb8451f5879b8f879d28c696c9719a5af5
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-16 11:21:54 -0300

    feat: cria middleware de cadastro de customer

---
+++ src/middlewares/customer/validatePostCustomer.ts
@@ -0,0 +1,27 @@
+import { Response, NextFunction, Request } from "express";
+import InvalidDataException from "../../exceptions/InvalidDataException";
+
+export const validatePostCustomer = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const {
+      name,
+      email,
+      cep,
+      city,
+      state
+    } = req.body;
+
+    if (!name || !email || !cep || !city || !state) {
+      throw new InvalidDataException(
+        "INVALID_DATA", "Ausência de dados obeigatórios na requisição"
+      );
+    }
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+};
--- src/routes/customerRoutes.ts
+++ src/routes/customerRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import CustomerController from "../controllers/customerController";
+import { validatePostContentCustomer } from "../middlewares/customer/validatePostContent";
+import { validatePostCustomer } from "../middlewares/customer/validatePostCustomer";
+import { validateTypePostCustomer } from "../middlewares/customer/validateTypePostCustomer";
 
 const customerRouter = Router();
 const customerController = new CustomerController();
 
 customerRouter.route("/").post(
+  validatePostCustomer,
+  validateTypePostCustomer,
+  validatePostContentCustomer,
   customerController.create.bind(customerController)
 );
 
