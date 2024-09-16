7c569ee970ba5882785477a05a54df056616d00b
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-16 00:03:41 -0300

    feat: cria customer controller

---
+++ src/controllers/customerController.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import { httpStatus } from "../utils/httpStatus.utils";
+
+export default class CustomerController {
+
+  constructor(
+    private customerService = new CustomerService()
+  ) { }
+
+  public async create(req: Request, res: Response, next: NextFunction
+  ) {
+    try {
+      const data = req.body;
+
+      const response = await this.customerService.create(data);
+      return res.status(httpStatus.CREATED).json(
+        {
+          customerCode: response.customerUuid
+        }
+      );
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+}
--- src/routes/customerRoutes.ts
+++ src/routes/customerRoutes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import CustomerController from "../controllers/customerController";
 
 const customerRouter = Router();
+const customerController = new CustomerController();
 
 customerRouter.route("/").post(
   customerController.create.bind(customerController)
